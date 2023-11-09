84d5345141fd7e09e9ea6a78d1abf53bff12b923
Kyle M. Brown <mayvik@gmail.com>
2023-11-09 06:00:14 -0500

    Added name tag to instance

--- main.tf
+++ main.tf
@@ -3,12 +3,15 @@ region = "us-east-1" // Book example is us-east-2
 }
 
 resource "aws_instance" "example" {
+  ami               = "ami-06aa3f7caf3a30282" // Using an us-east-1 similar ami  
   instance_type     = "t2.micro"
-//  availability_zone = "us-east-2a"
-  ami               = "ami-06aa3f7caf3a30282" // Using an us-east-1 similar ami
 
-//  user_data = <<-EOF
-//              #!/bin/bash
-//              sudo service apache2 start
-//              EOF
+  tags = {
+    Name = "terraform-example"
+  }  
+  #user_data = <<-EOF
+  #            #!/bin/bash
+  #            echo "Hello, World" > index.html
+  #            nohup busybox httpd -f -p 8080 &
+  #            EOF
 }
