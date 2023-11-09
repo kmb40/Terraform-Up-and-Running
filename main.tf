cc4219f2c2b5785ab4f053251a02d9c13449900b
Kyle M. Brown <mayvik@gmail.com>
2023-11-08 19:05:57 -0500

    Added instance and exported env variables

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
-region = "us-east-2"
+region = "us-east-1" // Book example is us-east-2
+}
+
+resource "aws_instance" "example" {
+  instance_type     = "t2.micro"
+//  availability_zone = "us-east-2a"
+  ami               = "ami-06aa3f7caf3a30282" // Using an us-east-1 similar ami
+
+//  user_data = <<-EOF
+//              #!/bin/bash
+//              sudo service apache2 start
+//              EOF
 }
