f7c7cbe9675ca2364e65550b4957fffcfa254baa
Kyle M. Brown <mayvik@gmail.com>
2023-11-13 15:35:36 -0500

    Added - Ch 3 - Isolation via Workspaces

---
+++ Ch-3/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1" // Book example is us-east-2
+}
+
+resource "aws_instance" "example" {
+  ami               = "ami-06aa3f7caf3a30282" // Using an us-east-1 similar ami  
+  instance_type     = "t2.micro"
+}
+
+terraform {
+backend "s3" {
+# Replace this with your bucket name!
+bucket = "terraform-up-and-running-state-kmb2"
+key = "workspaces-example/terraform.tfstate"
+region = "us-east-1"
+
+# Replace this with your DynamoDB table name!
+dynamodb_table = "terraform-up-and-running-locks"
+encrypt = true
+}
+}
