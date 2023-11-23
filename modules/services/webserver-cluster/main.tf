7a3aae6700f7689a88a80db37ce00adaa3087b17
Kyle M. Brown <mayvik@gmail.com>
2023-11-23 14:49:27 -0500

    Added - Chapter 4 modules

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -25,36 +25,6 @@ provider "aws" {
   region = "us-east-1" // Book example is us-east-2
 }
 
-# Variables
-variable "server_port" {
-  description = "The port the server will use for HTTP requests"
-  type = number
-  default = 8080
-}
-
-variable "security_group_name" {
-  description = "The name of the security group"
-  type        = string
-  default     = "terraform-example-instance"
-}
-
-variable "alb_name" {
-  description = "The application load balancer name"
-  type        = string
-  default     = "terraform-alb-example-instance"
-}
-
-variable "alb_security_group_name" {
-  description = "The name of the application load balancer security group"
-  type        = string
-  default     = "terraform-alb-sg-example-instance"
-}
-
-variable "bucket_name" {
-  description = "The name of the S3 bucket"
-  type        = string
-  default     = "terraform-up-and-running-state-kmb2"
-}
 
 //Start of bucket
 
@@ -263,9 +233,4 @@ resource "aws_security_group" "instance" {
     tags = {
     Name = "terraform-example-instance-sg"
   }  
-}
-
-output "alb_dns_name" {
-  value       = aws_lb.example.dns_name
-  description = "The domain name of the load balancer"
 }
